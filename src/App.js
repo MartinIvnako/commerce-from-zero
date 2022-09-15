45968be2755b811e3cfad110eb8ffecc499f5603
Martin Ivanko <martinivanko1238@gmail.com>
2022-09-15 12:37:05 +0200

    add and init Recoil

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import { Route, Routes } from "react-router-dom";
+import { RecoilRoot } from "recoil";
+
 import Navigation from "./components/navigation/navigation.component";
 import Checkout from "./routes/checkout.component";
 import Home from "./routes/home.component";
@@ -8,14 +10,16 @@ import SignUp from "./routes/sign-up.component";
 
 export default function App() {
     return (
-        <Routes>
-            <Route path="/" element={<Navigation />}>
-                <Route index element={<Home />} />
-                <Route path="/shop" element={<Shop />} />
-                <Route path="sign-in" element={<SignIn />} />
-                <Route path="sign-up" element={<SignUp />} />
-                <Route path="checkout" element={<Checkout />} />
-            </Route>
-        </Routes>
+        <RecoilRoot>
+            <Routes>
+                <Route path="/" element={<Navigation />}>
+                    <Route index element={<Home />} />
+                    <Route path="/shop" element={<Shop />} />
+                    <Route path="sign-in" element={<SignIn />} />
+                    <Route path="sign-up" element={<SignUp />} />
+                    <Route path="checkout" element={<Checkout />} />
+                </Route>
+            </Routes>
+        </RecoilRoot>
     );
 }
